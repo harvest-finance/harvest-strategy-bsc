59514d8878f2084392556022fb6eb17a32b05613
JasperSlimmens <jasper@slimmens.nl>
2021-03-23 10:36:25 +0100

    Added pancake bdo/bnb strategy.

---
+++ contracts/strategies/pancakeswap/PancakeStrategyMainnet_BDO_BNB.sol
@@ -0,0 +1,32 @@
+pragma solidity 0.6.12;
+
+import "../../base/pancake-base/PancakeMasterChefStrategy.sol";
+
+contract PancakeStrategyMainnet_BDO_BNB is PancakeMasterChefStrategy {
+
+  address public bdo_bnb_unused; // just a differentiator for the bytecode
+
+  constructor() public {}
+
+  function initializeStrategy(
+    address _storage,
+    address _vault
+  ) public initializer {
+    address underlying = address(0x74690f829fec83ea424ee1F1654041b2491A7bE9); //Cake-LP
+    address bdo = address(0x190b589cf9Fb8DDEabBFeae36a813FFb2A702454);
+    address cake = address(0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82);
+    address wbnb = address(0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c);
+    PancakeMasterChefStrategy.initializeStrategy(
+      _storage,
+      underlying,
+      _vault,
+      address(0x73feaa1eE314F8c655E354234017bE2193C9E24E), // master chef contract
+      cake,
+      66,  // Pool id
+      true // is LP asset
+    );
+    // bdo is token0, wbnb is token1
+    pancakeswapRoutes[bdo] = [cake, wbnb, bdo];
+    pancakeswapRoutes[wbnb] = [cake, wbnb];
+  }
+}
