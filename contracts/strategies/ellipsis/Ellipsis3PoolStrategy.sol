6f4969126014987930b3fcf4f45475ee5277239a
JasperSlimmens <jasper@slimmens.nl>
2021-03-31 07:48:16 +0200

    Updated PS to 8%

--- contracts/strategies/ellipsis/Ellipsis3PoolStrategy.sol
+++ contracts/strategies/ellipsis/Ellipsis3PoolStrategy.sol
@@ -51,7 +51,7 @@ contract Ellipsis3PoolStrategy is BaseUpgradeableStrategy {
       _vault,
       _rewardPool,
       _rewardToken,
-      300, // profit sharing numerator
+      80, // profit sharing numerator
       1000, // profit sharing denominator
       true, // sell
       1e16, // sell floor
