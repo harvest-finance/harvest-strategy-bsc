04f153f95396d135d80f980d355269f1ee3e11ca
JasperSlimmens <jasper@slimmens.nl>
2021-04-13 21:02:12 +0200

    Space strategies with deposit fee check

---
+++ contracts/base/masterchef/interface/IMasterChefDepositFee.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.6.12;
+
+interface IMasterChefDepositFee {
+    function deposit(uint256 _pid, uint256 _amount) external;
+    function withdraw(uint256 _pid, uint256 _amount) external;
+    function userInfo(uint256 _pid, address _user) external view returns (uint256 amount, uint256 rewardDebt);
+    function poolInfo(uint256 _pid) external view returns (address lpToken, uint256, uint256, uint256, uint16);
+    function massUpdatePools() external;
+}
