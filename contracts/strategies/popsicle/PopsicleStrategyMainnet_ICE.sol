3c80a4fca5d3109a9de39e925a9c947995572de7
JasperSlimmens <jasper@slimmens.nl>
2021-04-27 19:38:37 +0200

    Update Popsicle

--- contracts/strategies/popsicle/PopsicleStrategyMainnet_ICE.sol
+++ contracts/strategies/popsicle/PopsicleStrategyMainnet_ICE.sol
@@ -2,9 +2,9 @@
 
 pragma solidity 0.6.12;
 
-import "../../base/masterchef/GeneralMasterChefStrategy.sol";
+import "./PopsicleStrategy.sol";
 
-contract PopsicleStrategtMainnet_ICE is GeneralMasterChefStrategy {
+contract PopsicleStrategtMainnet_ICE is PopsicleStrategy {
 
   address public ice_unused; // just a differentiator for the bytecode
 
@@ -15,14 +15,17 @@ contract PopsicleStrategtMainnet_ICE is GeneralMasterChefStrategy {
     address _vault
   ) public initializer {
     address ice = address(0xf16e81dce15B08F326220742020379B855B87DF9);
-    GeneralMasterChefStrategy.initializeStrategy(
+    address wbnb = address(0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c);
+    PopsicleStrategy.initializeStrategy(
       _storage,
       ice,
       _vault,
       address(0x05200cB2Cee4B6144B2B2984E246B52bB1afcBD0), // master chef contract
-      ice,
+      wbnb,
       0,  // Pool id
       false // is LP asset
     );
+    ice2bnb = [ice, wbnb];
+    sushiswapRoutes[ice] = [wbnb, ice];
   }
 }
