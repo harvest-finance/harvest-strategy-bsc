9834e560c5869fd7ebd6136ac30fe4a139b61b31
JasperSlimmens <jasper@slimmens.nl>
2021-04-27 19:38:37 +0200

    Update Popsicle

--- contracts/strategies/popsicle/PopsicleStrategyMainnet_ICE_BNB.sol
+++ contracts/strategies/popsicle/PopsicleStrategyMainnet_ICE_BNB.sol
@@ -2,9 +2,9 @@
 
 pragma solidity 0.6.12;
 
-import "../../base/masterchef/GeneralMasterChefStrategy.sol";
+import "./PopsicleStrategy.sol";
 
-contract PopsicleStrategtMainnet_ICE_BNB is GeneralMasterChefStrategy {
+contract PopsicleStrategtMainnet_ICE_BNB is PopsicleStrategy {
 
   address public ice_bnb_unused; // just a differentiator for the bytecode
 
@@ -14,18 +14,19 @@ contract PopsicleStrategtMainnet_ICE_BNB is GeneralMasterChefStrategy {
     address _storage,
     address _vault
   ) public initializer {
-    address underlying = address(0xFE3171B9c20d002376D4B0097207EDf54b02EA3B);
+    address underlying = address(0x51F914a192a97408D991FddDAFB8F8537C5Ffb0a);
     address ice = address(0xf16e81dce15B08F326220742020379B855B87DF9);
     address wbnb = address(0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c);
-    GeneralMasterChefStrategy.initializeStrategy(
+    PopsicleStrategy.initializeStrategy(
       _storage,
       underlying,
       _vault,
       address(0x05200cB2Cee4B6144B2B2984E246B52bB1afcBD0), // master chef contract
-      ice,
-      1,  // Pool id
+      wbnb,
+      2,  // Pool id
       true // is LP asset
     );
-    pancakeswapRoutes[wbnb] = [ice, wbnb];
+    ice2bnb = [ice, wbnb];
+    sushiswapRoutes[ice] = [wbnb, ice];
   }
 }
